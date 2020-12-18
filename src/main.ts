e286b23f56e5c1a0f86d9c9d039cd142cc6b2a45
Pongkot Sae-li <pongkot.saeli@outlook.com>
2020-12-18 09:48:51 +0700

    create method

--- src/main.ts
+++ src/main.ts
@@ -31,17 +31,30 @@ const input = {
   ],
 };
 
-let result = "";
+let result: string = "";
 const templateClass = "export class [className] { [properties] [method] }";
 
 result = templateClass.replace("[className]", input.className);
 
-const properties = input.properties
+const properties: string = input.properties
   .map((i: { name: string; type: string }) => {
     return `private ${i.name}: ${i.type};`;
   })
-  .join("");
+  .join(" ");
+
+const methods: string = input.properties
+  .map((i: { name: string; type: string }) => {
+    return {
+      getter: `get${i.name}(): ${i.type} { return this.${i.name}; };`,
+      setter: `set${i.name}(${i.name}: ${i.type}): this { this.${i.name} = ${i.name}; return this; };`,
+    };
+  })
+  .map((i) => {
+    return [i.getter, i.setter].join(" ");
+  })
+  .join(" ");
 
 result = result.replace("[properties]", properties);
+result = result.replace("[method]", methods);
 
 showHello("greeting", result);
